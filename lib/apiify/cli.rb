f542555ae002a8ee8773cfb7097485319e1966d0
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2017-01-19 11:30:33 +0000

    Amended generate method to rename as new, and improved description and output message

--- lib/apiify/cli.rb
+++ lib/apiify/cli.rb
@@ -3,14 +3,20 @@ require 'apiify'
 
 class Apiify::CLI < Thor
 
-  desc "generate", "should build scaffold"
-  def generate(csv_path)
+  desc "new", "extracts file name and creates a model and properties based on
+    CSV file name and header columns"
+  def new(csv_path)
     scaffolder = Apiify::Scaffolder.new
     system(scaffolder.create_scaffold(csv_path))
-    system("bin/rake db:migrate")
+    # system("bin/rake db:migrate") - we ask the user to do this bit themselves
+    puts "#{scaffolder.get_file_name(csv_path).capitalize} model with properties
+      #{scaffolder.hash_to_string(find_class(csv_path))} created"
+  end
+
+  def import(csv_path)
     importer = Apiify::CsvImporter.new
     importer.import_csv(csv_path)
-    puts "success"
+    puts "Import successful"
   end
 
 
