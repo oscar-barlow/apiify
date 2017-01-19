4a85d4935cb4fc846c2d5113f9120c14a1f8acd7
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2017-01-19 12:00:56 +0000

    Updated gem

--- spec/csv_importer_spec.rb
+++ spec/csv_importer_spec.rb
@@ -1,14 +1,21 @@
 require "spec_helper"
 require 'apiify'
+require_relative '../scaffolder'
 
 describe Apiify::CsvImporter do
 
   subject(:csv_importer) {described_class.new}
+  subject(:scaffolder) { Apiify::Scaffolder.new }
   let(:csv_path) {"fixtures/bunny.csv"}
 
   it "returns the filename of the csv file" do
-    expect(subject.get_file_name(csv_path)).to eq("bunny")
+    expect(csv_importer.get_file_name(csv_path)).to eq("bunny")
   end
+  # 
+  # it "creates a new record for every line in CSV file" do
+  #   exec(scaffolder.create_scaffold(csv_path))
+  #   expect { csv_importer.import_csv }.to change {Bunny.count}.by 12
+  # end
 
 
 
